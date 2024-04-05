2ba03e6ffd5baa8d806eff7688a4fb7829e18377
Kev Informatica <kvn.guitar7@gmail.com>
2024-04-05 13:22:07 -0600

    Codigo para parpadeo de led y conversion de adc

---
+++ convertirADCaVolts/convertirADCaVolts.ino
@@ -0,0 +1,19 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600); // con esta instruccion se mostraran los resultados en el monitor serial
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int adc = analogRead(A1); // Guarda el valor de ADC en el pin A1 y se declara la variable como tipo entero
+  int volt = adc * 5 / 1023; //Esta operación convierte el valor que se recibe en el ADC y lo convierte al voltaje se declara la variable como tipo entero
+
+  Serial.print("El valor del ADC es: ");
+  Serial.print(adc);
+  Serial.print(" , y el voltaje es: ");
+  Serial.print(volt);
+  Serial.print(" voltios");
+
+  delay(1000); // Retardo de 1 segundo entre lecturas
+}
--- parpadeoled.ino
+++ parpadeoled.ino
@@ -6,8 +6,5 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(13, HIGH); //Envia la instruccion para encender el LED
-  delay(500); //Retardo de medio segundo
-  digitalWrite(13, LOW); //Envia instruccion para apagar el LED
-  delay(500); //Retardo de medio segundo
+  digitalWrite(13, (millis() / 1000) % 2); // se envia como parametro el millis dentro de 1000, para que cada vez que alcance un multiplo de 1000, con el mod 2 solo nos dara como resultado un 1 o 0, que se interpreta como 1 para HIGH y 0 para LOW
 }
